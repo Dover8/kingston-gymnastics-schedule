b21ce3b4144762cee0aea584320c7f998f26e711
Ben MacKinnon <ben.mackinnon@soluis.com>
2019-11-05 17:20:25 +0000

    Added date picker for week selection

---
+++ assets/js/DateDemo.js
@@ -0,0 +1,18 @@
+jQuery(document).ready(function(){
+    moment.locale('en', {
+      week: { dow: 1 } // Monday is the first day of the week
+    });
+
+  //Initialize the datePicker(I have taken format as mm-dd-yyyy, you can     //have your owh)
+  $("#weeklyDatePicker").datetimepicker({
+      format: 'MM-DD-YYYY'
+  });
+
+   //Get the value of Start and End of Week
+  $('#weeklyDatePicker').on('dp.change', function (e) {
+      var value = $("#weeklyDatePicker").val();
+      var firstDate = moment(value, "MM-DD-YYYY").day(1).format("MM-DD-YYYY");
+      var lastDate =  moment(value, "MM-DD-YYYY").day(7).format("MM-DD-YYYY");
+      $("#weeklyDatePicker").val(firstDate + " - " + lastDate);
+  });
+});
