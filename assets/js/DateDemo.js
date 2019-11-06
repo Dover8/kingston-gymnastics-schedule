e4fb48833fcc2ab8adf0c2ace78b6e840cd9ef90
Ben MacKinnon <ben.mackinnon@soluis.com>
2019-11-06 11:30:02 +0000

    Switched date format to UK standard

--- assets/js/DateDemo.js
+++ assets/js/DateDemo.js
@@ -5,14 +5,14 @@ jQuery(document).ready(function(){
 
   //Initialize the datePicker(I have taken format as mm-dd-yyyy, you can     //have your owh)
   $("#weeklyDatePicker").datetimepicker({
-      format: 'MM-DD-YYYY'
+      format: 'DD-MM-YYYY'
   });
 
    //Get the value of Start and End of Week
   $('#weeklyDatePicker').on('dp.change', function (e) {
       var value = $("#weeklyDatePicker").val();
-      var firstDate = moment(value, "MM-DD-YYYY").day(1).format("MM-DD-YYYY");
-      var lastDate =  moment(value, "MM-DD-YYYY").day(7).format("MM-DD-YYYY");
+      var firstDate = moment(value, "DD-MM-YYYY").day(1).format("DD-MM-YYYY");
+      var lastDate =  moment(value, "DD-MM-YYYY").day(7).format("DD-MM-YYYY");
       $("#weeklyDatePicker").val(firstDate + " - " + lastDate);
   });
 });
