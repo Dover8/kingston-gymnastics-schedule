5be094f719bb061380c6210e995b672da6309344
Ben MacKinnon <ben.mackinnon@soluis.com>
2019-11-07 15:35:54 +0000

    Added and styles a date picker
    For selecting the week to view events for

--- assets/js/DateDemo.js
+++ assets/js/DateDemo.js
@@ -4,15 +4,15 @@ jQuery(document).ready(function(){
     });
 
   //Initialize the datePicker(I have taken format as mm-dd-yyyy, you can     //have your owh)
-  $("#weeklyDatePicker").datetimepicker({
+  $("#datepicker").datetimepicker({
       format: 'DD-MM-YYYY'
   });
 
    //Get the value of Start and End of Week
-  $('#weeklyDatePicker').on('dp.change', function (e) {
       var value = $("#weeklyDatePicker").val();
       var firstDate = moment(value, "DD-MM-YYYY").day(1).format("DD-MM-YYYY");
       var lastDate =  moment(value, "DD-MM-YYYY").day(7).format("DD-MM-YYYY");
       $("#weeklyDatePicker").val(firstDate + " - " + lastDate);
+  $('#datepicker').on('dp.change', function (e) {
   });
 });
